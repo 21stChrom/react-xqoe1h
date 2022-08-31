8097ebfa05350dbb496a9d8e170a73518576e1ca
21stChrom <parkerjmartin2020@gmail.com>
2022-08-31 00:21:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xqoe1h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xqoe1h)
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./style.css";
+const audio = new Audio(this.url);  audio.addEventListener('ended', function () {
+  this.currentTime = 0;
+  this.play();
+}, false);
+audio.loop = true;
+audio.play().then(()=>{});
+export default function App() {
+  return (
+    <div class="container">
+      <h1>TIME</h1>
+      <audio controls loop>
+        <source src="https://cdn.jsdelivr.net/gh/21stChrom/practice-projects@main/src/Juice%20WRLD%20-%20I%20Hate%20Wasting%20Time%20%20%20(Unreleased)%20[prod.%20dfk]%20[720p].mp3" type="audio/mpeg "/>
+      </audio>
+    
+      <div></div>
+      <p><span style={{fontWeight: 'bold'}}>{Date()}</span></p>
+    </div>
+  );
+}
