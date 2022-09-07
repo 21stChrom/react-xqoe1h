bec7f8d4b5bf44288ae056e426d5dfd7ae7cb11f
21stChrom <81393334+21stChrom@users.noreply.github.com>
2022-09-06 22:05:58 -0400

    1 file modified

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ export default function App() {
         </audio><audio controls loop>
         <source src="https://cdn.jsdelivr.net/gh/21stChrom/react-xqoe1h@main/src/Juice%20WRLD%20-%20Traumatized%20(%20Lyrics%20)%20[%20Unreleased%20]%20[%20Prod.%20by%20RockyRoadz%20]%20__%20Vibe%20Alone%20[720p].mp3" type="audio/mpeg" data-track-number="2"/>
       </audio>
-      <div></div>
+      <div>
+        <iframe src="https://react-at9jyv.stackblitz.io/"></iframe>
+      </div>
       <p><span style={{fontWeight: 'bold'}}>{Date()}</span></p>
     </div>
   );
